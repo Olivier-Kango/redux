7e4982935a6a84838feb0c8d6fed4b5e2fbc3b35
Olivier-Kango <olivierkango@gmail.com>
2022-11-14 10:04:20 -0800

    Add the reducer component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import './App.css';
 // npm install redux
 // npm install react-redux
 // npm install --save redux-devtools-extension
+// Use store, actions, and reducers in Redux
+
+const reducers = (state, action) => {
+  switch(action.type) {
+    case "TYPE": return {}
+  }
+}
 
 function App() {
   return (
